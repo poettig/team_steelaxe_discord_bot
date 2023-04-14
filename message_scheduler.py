1c6f8f278b2f1aee344d8a86441c84f4bc3cb0d1
Peter Oettig <peet1993@gmail.com>
2023-04-14 02:32:22 +0200

    Allow empty and nonexistant scheduled message file

--- message_scheduler.py
+++ message_scheduler.py
@@ -1,3 +1,4 @@
+import logging
 import os.path
 
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -9,15 +10,19 @@ from discord_client import DiscordClient
 
 class MessageScheduler:
 	def __init__(self, scheduled_message_file: str, discord_client: DiscordClient):
-		if not os.path.isfile(scheduled_message_file):
-			raise ValueError("The file with the scheduled messages does not exist.")
-
-		with open(scheduled_message_file, 'r') as fh:
-			self.scheduled_messages = yaml.safe_load(fh)
-
 		self.discord_client = discord_client
+		self.scheduled_messages = []
+
+		if not os.path.isfile(scheduled_message_file):
+			logging.warning("The file with the scheduled messages does not exist, will not schedule any messages.")
+		else:
+			with open(scheduled_message_file, 'r') as fh:
+				self.scheduled_messages = yaml.safe_load(fh)
 
 	def start(self):
+		if len(self.scheduled_messages) == 0:
+			return
+
 		scheduler = BlockingScheduler()
 
 		for scheduled_message in self.scheduled_messages:
